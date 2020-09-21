ecdc370226206e012857b0b06e7eaa64af17cfe9
Krittakan <ragunakan@gmail.com>
2020-09-21 13:56:19 +0700

    remove dashboard

--- client/src/_nav.js
+++ client/src/_nav.js
@@ -1,11 +1,6 @@
 const backendNav = {
   items: [
     {
-      name: 'Dashboard',
-      url: '/dashboard',
-      icon: 'fa fa-star'
-    },
-    {
       name: 'Model',
       url: '/model',
       icon: 'fa fa-star'
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,4 @@
 import React from 'react';
-const Dashboard = React.lazy(() => import('./views/Dashboard/Dashboard'));
 
 const ModelList = React.lazy(() => import('./views/Model/List'));
 const ModelAdd = React.lazy(() => import('./views/Model/Add'));
@@ -10,9 +9,6 @@ const routes = [
   { path: '/model', exact: true, name: 'Model List', component: ModelList },
   { path: '/model/add', exact: true, name: 'Model Add', component: ModelAdd },
   { path: '/model/:id', exact: true, name: 'Model Detail', component: ModelDetail },
-  
-  { path: '/dashboard', name: 'Dashboard', component: Dashboard },
-  
 ];
 
 export default routes;
