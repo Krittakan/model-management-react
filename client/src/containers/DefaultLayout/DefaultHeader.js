81fccaaff75ebbae8afb1c2d54870f69fe97a9c0
Krittakan <ragunakan@gmail.com>
2020-09-22 10:05:17 +0700

    remove profile

--- client/src/containers/DefaultLayout/DefaultHeader.js
+++ client/src/containers/DefaultLayout/DefaultHeader.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem } from 'reactstrap';
 import PropTypes from 'prop-types';
 
 import { AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
@@ -26,19 +25,6 @@ class DefaultHeader extends Component {
           full={{ src: logo, width: 89, height: 55, alt: 'CoreUI Logo' }}
           minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
         />
-        <Nav className="ml-auto" navbar>
-          <NavItem className="d-md-down-none text-right">
-          </NavItem>
-          <UncontrolledDropdown nav direction="down">
-            <DropdownToggle nav>
-              <img src={'../../assets/img/avatars/6.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
-            </DropdownToggle>
-            <DropdownMenu right>
-              <DropdownItem header tag="div" className="text-center"><strong>User</strong></DropdownItem>
-              <DropdownItem onClick={e => this.props.onLogout(e)}><i className="fa fa-lock"></i> Logout</DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-        </Nav>
       </React.Fragment>
     );
   }
